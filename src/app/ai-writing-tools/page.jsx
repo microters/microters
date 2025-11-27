822adec1e66677ac85a5b223437dffc8ea589252
codershawon <shawonb500@gmail.com>
2025-11-27 14:52:28 +0600

    `Added AiWritingToolsPage component`

---
+++ src/app/ai-writing-tools/page.jsx
@@ -0,0 +1,11 @@
+export default function AiWritingToolsPage() {
+  return (
+    <main>
+      <div className="bg-[#fef7f5] pt-16 lg:pt-20">
+        <h1 className="text-[#212E4A] text-4xl lg:text-7xl text-center font-extrabold leading-tight">
+          This is Ai Writing Page
+        </h1>
+      </div>
+    </main>
+  );
+}
