552d817e3345e1b7bd4268bc488eee2c9efc7afc
codershawon <shawonb500@gmail.com>
2026-05-25 17:31:29 +0600

    Update next.config.mjs to enable image optimization settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'api.microters.com',
+        pathname: '/uploads/**',
       },
     ],
   },
